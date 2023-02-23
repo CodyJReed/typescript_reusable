c7e4c35898b3fc186a8270035da5f7c51b5ee904
CodyJReed <cody2j2reed@gmail.com>
2023-02-22 21:16:00 -0800

    type_reuse finish first iteration... not reusable

--- build/index.js
+++ build/index.js
@@ -1,2 +1,16 @@
 "use strict";
-console.log("cody + stephen grider");
+Object.defineProperty(exports, "__esModule", { value: true });
+const CsvFileReader_1 = require("./CsvFileReader");
+const MatchResult_1 = require("./MatchResult");
+const reader = new CsvFileReader_1.CsvFileReader("football.csv");
+reader.read();
+console.log(reader.data[0][0]);
+let manUnitedWins = 0;
+const { HomeWin, AwayWin } = MatchResult_1.MatchResult;
+for (let match of reader.data) {
+    if (match[1] === 'Man United' && match[5] === HomeWin)
+        manUnitedWins++;
+    if (match[2] === 'Man United' && match[5] === AwayWin)
+        manUnitedWins++;
+}
+console.log(`Man wins ${manUnitedWins}`);
