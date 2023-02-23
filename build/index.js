f7ab1a7ac980d4e04adf5f79a87e6ebede12da71
CodyJReed <cody2j2reed@gmail.com>
2023-02-22 22:31:07 -0800

    type_reuse finish third iteration using composition

--- build/index.js
+++ build/index.js
@@ -1,12 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const MatchReader_1 = require("./MatchReader");
+const CsvFileReader_1 = require("./CsvFileReader");
 const MatchResult_1 = require("./MatchResult");
-const reader = new MatchReader_1.MatchReader("football.csv");
-reader.read();
+const csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
+const matchReader = new MatchReader_1.MatchReader(csvFileReader);
+matchReader.load();
 let manUnitedWins = 0;
 const { HomeWin, AwayWin } = MatchResult_1.MatchResult;
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
     if (match[1] === "Man United" && match[5] === HomeWin)
         manUnitedWins++;
     if (match[2] === "Man United" && match[5] === AwayWin)
