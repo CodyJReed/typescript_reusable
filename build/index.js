e1bb931d0c9a1d54d9cff2251496531f2d164a1a
CodyJReed <cody2j2reed@gmail.com>
2023-02-22 22:10:59 -0800

    type_reuse finish up second iteration... is now reusable

--- build/index.js
+++ build/index.js
@@ -1,16 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const CsvFileReader_1 = require("./CsvFileReader");
+const MatchReader_1 = require("./MatchReader");
 const MatchResult_1 = require("./MatchResult");
-const reader = new CsvFileReader_1.CsvFileReader("football.csv");
+const reader = new MatchReader_1.MatchReader("football.csv");
 reader.read();
-console.log(reader.data[0][0]);
 let manUnitedWins = 0;
 const { HomeWin, AwayWin } = MatchResult_1.MatchResult;
 for (let match of reader.data) {
-    if (match[1] === 'Man United' && match[5] === HomeWin)
+    if (match[1] === "Man United" && match[5] === HomeWin)
         manUnitedWins++;
-    if (match[2] === 'Man United' && match[5] === AwayWin)
+    if (match[2] === "Man United" && match[5] === AwayWin)
         manUnitedWins++;
 }
 console.log(`Man wins ${manUnitedWins}`);
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,16 @@
-import { CsvFileReader } from "./CsvFileReader"
-import { MatchResult } from "./MatchResult"
+import { MatchReader } from "./MatchReader";
+import { MatchResult } from "./MatchResult";
 
-const reader = new CsvFileReader("football.csv")
-reader.read()
+const reader = new MatchReader("football.csv");
+reader.read();
 
-console.log(reader.data[0][0])
+let manUnitedWins = 0;
 
-let manUnitedWins = 0
-
-const {HomeWin, AwayWin} = MatchResult
+const { HomeWin, AwayWin } = MatchResult;
 
 for (let match of reader.data) {
-    if (match[1] === 'Man United' && match[5] === HomeWin) manUnitedWins++
-    if (match[2] === 'Man United' && match[5] === AwayWin) manUnitedWins++
+  if (match[1] === "Man United" && match[5] === HomeWin) manUnitedWins++;
+  if (match[2] === "Man United" && match[5] === AwayWin) manUnitedWins++;
 }
 
-console.log(`Man wins ${manUnitedWins}`)
+console.log(`Man wins ${manUnitedWins}`);
